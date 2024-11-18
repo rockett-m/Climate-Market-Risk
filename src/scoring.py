1b5c56ff64459f341a87c7783f1e81cb53c68f38
Morgan Rockett <morgan.rockett@tufts.edu>
2024-11-17 21:01:45 -0500

    scoring gets past warming calcs for co2, working on future warming

--- src/scoring.py
+++ src/scoring.py
@@ -3,6 +3,8 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
+import math
 
 
 ROOT = os.path.abspath(os.path.dirname(os.path.join(__file__, "../../")))
@@ -14,29 +16,14 @@ def path_check(path_name: str) -> bool:
     return True
 
 
-def calc_climate_score(df_emissions: pd.DataFrame) -> float:
-    """
-    inputs:
-        dataframe with aggregate emissions for last 100 years (in tons)
-        CO2 carbon dioxide
-        CH4 methane
-        N2O nitrous oxide
-    output:
-        climate risk score
-    """
-    climate_score = 0.0
-
-    return climate_score
-
-
 def parse_culm_gas_emissions() -> pd.DataFrame:
     """
-    col_headers = ["Year", "CO2‚RF (W/m^2)", "CO2 kWh", "CO2 Aggregate warming", \
-                "CH4‚RF (W/m^2)", "CH4 kWh", "CH4 Aggregate warming", \
-                "N2O‚RF (W/m^2)", "N2O kWh", "N2O Aggregate warming"]
+    col_headers = ["Year", "CO2 RF (W/m^2)", "CO2 kWh", "CO2 Aggregate warming", \
+                "CH4 RF (W/m^2)", "CH4 kWh", "CH4 Aggregate warming", \
+                "N2O RF (W/m^2)", "N2O kWh", "N2O Aggregate warming"]
     """
     data_historical = os.path.join(ROOT,
-        "resources/Radiative_Efficiency_Over_100_Y.csv")
+        "data/emissions_data/Radiative_Efficiency_Over_100_Y.csv")
     path_check(data_historical)
     df = pd.read_csv(data_historical)
 
@@ -62,8 +49,90 @@ def parse_culm_gas_emissions() -> pd.DataFrame:
     return df
 
 
+def calc_climate_score(
+        df_emissions: pd.DataFrame,
+        year_company_added_to_sp500=1988
+    ) -> float:
+    """
+    inputs:
+        dataframe with aggregate emissions for last 100 years (in tons)
+        CO2 carbon dioxide
+        CH4 methane
+        N2O nitrous oxide
+    output:
+        climate risk score
+    """
+
+    """
+    calculate the non-worn off global warming that is resultant of the company
+
+    Radiative Forcing
+        510.1km^2 = surface area of the earth
+        = 510.1 * 1E6 for m^2
+        510.1km^2 * 10E12 Watts for TeraWatts
+
+    For each W/m^2, there is a 510E12 W increase in heat rate level
+    """
+    surface_area_earth_m_sq = 510.1 * 1E12
+    past_emissions_carb_diox = 0.0
+    years_til_1pct_thresh = 4610
+
+    latest_year = df_emissions["Year"].iloc[0].astype(int) # 2023
+    # print(latest_year, type(latest_year))
+    idx = 0
+    for curr_year in range(int(latest_year), year_company_added_to_sp500 + 1, -1):
+        # year_company_added_to_sp500, int(latest_year) + 1):
+
+        rf_co2_year = df_emissions.iloc[idx]["CO2 RF (W/m^2)"]
+
+        year_carb_diox_rf = rf_co2_year * surface_area_earth_m_sq * \
+            0.001 * 24 * 365.25
+        print(f"year {curr_year} warming {year_carb_diox_rf}")
+        past_emissions_carb_diox += year_carb_diox_rf
+        print(f"aggregate warming so far: {past_emissions_carb_diox}")
+        idx += 1
+
+    print(f"\nwarming from {past_emissions_carb_diox = }\n")
+
+    # FUTURE CALCS til 4610 years of warming
+    # extrapolate to future, til 1% threshold is hit
+    future_warming_carb_diox = 0.0
+    alpha = 5.35 # W/m^2
+    conc_ppm = 280 # 284
+    conc_ref = 280 # ppm
+    r = 0.0057 # growth rate
+
+    for year in range(years_til_1pct_thresh):
+
+        t = year
+        # FIXME: co2 conc function C(t) = C_0(t) * e^(rt)
+        conc_ppm = conc_ppm * math.exp(r * t)
+
+        rf = alpha * math.log(conc_ppm / conc_ref)
+
+        # future_warming_carb_diox += rf
+
+        year_carb_diox_rf = rf * surface_area_earth_m_sq * 0.001 * 24 * 365.25
+        future_warming_carb_diox += year_carb_diox_rf
+
+
+    # print(f"{future_warming_carb_diox = }\n")
+
+    return past_emissions_carb_diox
+    # return past_emissions_carb_diox + future_warming_carb_diox
+
+
 if __name__ == "__main__":
 
     df_emissions = parse_culm_gas_emissions()
 
     calc_climate_score(df_emissions)
+
+
+
+""" Radioactive Force Decay (TWh)
+       CO2
+US    2.37E+10
+MSFT  1.91E+07
+GOOG  7.52E+10
+"""
